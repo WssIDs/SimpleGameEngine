0d4e0b6fda9ee931fcc3848dfb4762278c07d89b
WssIDs <wssids49@gmail.com>
2019-12-02 00:18:56 +0300

    feature: Euler angles

---
+++ Source/Graphics/DX11/Math/XM.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <Windows.h>
+#include <DirectXMath.h>
+
+// need rename
+struct XMatrix
+{
+	static FORCEINLINE DirectX::XMFLOAT3 GetEulerAngles(const DirectX::XMFLOAT4X4& mat)
+	{
+		DirectX::XMFLOAT3 euler;
+
+		euler.x = asinf(-mat._32);                  // Pitch
+		if (cosf(euler.x) > 0.0001)                // Not at poles
+		{
+			euler.y = atan2f(mat._31, mat._33);      // Yaw
+			euler.z = atan2f(mat._12, mat._22);      // Roll
+		}
+		else
+		{
+			euler.y = 0.0f;                           // Yaw
+			euler.z = atan2f(-mat._21, mat._11);     // Roll
+		}
+
+		return euler;
+	}
+
+	static FORCEINLINE DirectX::XMFLOAT3 GetTranslation(const DirectX::XMFLOAT4X4& matrix)
+	{
+		return { matrix._41,matrix._42,matrix._43 };
+	}
+};
